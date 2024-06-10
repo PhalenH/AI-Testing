91110d9b371e3b33d80afd580b0c21ec1a771071
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2024-06-09 22:28:10 -0400

    small updated in app.js to build out webpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="app">
+      <section className="side-nav"></section>
+      <button onclick="displayResult()">+ New prompt</button>
+      <ul className="historical"></ul>
+      <nav>
+        <p>Welcome back</p>
+      </nav>
+      <section className="main">
+        <h1>This is using OpenAI API!</h1>
+        <ul>
+          <li></li>
+        </ul>
+        <div>
+          <div className="container">
+            <input type="text" id="inputField" />
+            <button onclick="displayResult()">Submit</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
